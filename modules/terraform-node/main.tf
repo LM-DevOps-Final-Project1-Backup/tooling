a18ae5eed6d0f804d440e207a3437a10b7764bbc
gabriel <bajogab2@gmail.com>
2024-10-13 18:16:26 -0400

    updates

--- modules/terraform-node/main.tf
+++ modules/terraform-node/main.tf
@@ -4,7 +4,7 @@ resource "aws_instance" "terraform_node" {
   key_name      = var.key_name
   user_data     = file("${path.module}/terraform.sh")
 
-  ags = {
+  tags = {
     Name = "terraform-node"
   }
 }
