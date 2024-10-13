338a1adc8089b9fb53dc0ceb74625613d7e4b100
gabriel <bajogab2@gmail.com>
2024-10-13 16:55:43 -0400

    updates

--- modules/s3-dynamodb/variables.tf
+++ modules/s3-dynamodb/variables.tf
@@ -1,17 +1,17 @@
 variable "bucket" {
   description = "The name of the S3 bucket"
   type        = string
-  default     = "dominion-terraform-backend-bucket"
+  default     = "terraform-state-bucket-gabe01"
 }
 
 variable "table" {
   description = "The name of the DynamoDB table"
   type        = string
-  default     = "terraform-state-locking"
+  default     = "terraform-lock-table-gabe01"
 }
 
 variable "region" {
   description = "The AWS region"
   type        = string
-  default     = "us-west-2"
+  default     = "us-east-1"
 }
